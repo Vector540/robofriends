640bfa04300489310fddf120d4e988de875f4311
FM-504 <arieyv.xlive@gmail.com>
2022-07-27 20:06:55 -0300

    test

--- src/card.js
+++ src/card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({email, id, name}) =>{
     return(
         <div className='bg-light-green hover-light-silver dib br4 pa1 ma3 grow bw2 shadow-5'>
-            <img alt='Robots' src={`https://robohash.org/${id}?200x200`}/>
+            <img alt='Robots' src={`https://robohash.org/${id}?200x201`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
